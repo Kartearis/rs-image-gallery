11c0db9b59dcb80b9db32c0431b00a79378b8c39
Matveev Timofey <justshowel@gmail.com>
2022-02-14 22:13:37 +0300

    feat: Implement image loading and searching

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,6 +1,55 @@
 
-
+const seeds = ['cat', 'dog', 'mountain', 'river', 'sea', 'red', 'blue', 'dragon', 'rolling', 'scopes', 'school', 'black', 'fire', 'tree'];
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('year').innerHTML = new Date().getFullYear();
+});
+
+function processKeysOnSearch(event) {
+    if (event.code === 'Enter')
+        loadImages(document.querySelector('input.search').value);
+}
+
+function loadImages(query, page = 1) {
+    if (query.length < 2)
+        return;
+    fetch('https://env2k6glw6z34ld.m.pipedream.net?' + new URLSearchParams({
+        'query': query,
+        'page': page
+    }))
+    .then(response => {if (!response.ok) throw new Error("Server unavailable"); return response.json();})
+    .then(customResponse => {if (customResponse.status !== 0) throw new Error("Some API error"); return customResponse.data;})
+    .then(data => buildGallery(data.results.map(x => {return {full: x.urls.full, normal: x.urls.small, alt: x.alt_description};})))
+    .catch(er => displayError(er));
+}
+
+function displayError(error) {
+    console.log(error);
+
+}
+
+const imageHtml = `
+    <img src="" alt="">
+`;
+
+function buildGallery(images, totalImages) {
+    let gallery = document.querySelector('.gallery');
+    gallery.innerHTML = "";
+    images.forEach(x => {
+        const image = document.createElement('img');
+        image.src = x.normal;
+        image.attributes['data-full'] = x.full;
+        image.alt = x.alt;
+        gallery.append(image);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let search = document.querySelector('input.search');
+    search.addEventListener('keydown', processKeysOnSearch);
+    document.querySelector('button#search').addEventListener('click', x => loadImages(search.value))
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages(seeds[Math.floor(Math.random() * seeds.length)]);
 });
